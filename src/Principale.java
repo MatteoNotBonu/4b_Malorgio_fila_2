962bb6398e1fd6f76d9120dac839db3651e24b84
4b <4b@INF06>
2022-04-23 12:21:54 +0200

    versione 1.0

---
+++ src/Principale.java
@@ -0,0 +1,13 @@
+/*
+ * @author Malorgio Matteo 
+ * 
+ * version 1.0
+ */
+public class Principale {
+
+	public static void main(String[] args) {
+		System.out.println("Malorgio Matteo");
+
+	}
+
+}
